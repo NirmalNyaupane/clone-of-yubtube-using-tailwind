c01c758a43e1c7640f850e6db10b651b701464c1
Nirmal Neupane <101554702+NirmalNyaupane@users.noreply.github.com>
2023-04-28 12:46:37 +0545

    Delete Yubtube Clone - Copy directory

--- Yubtube Clone - Copy/src/script.js
+++
@@ -1,7 +0,0 @@
-const sideBar = document.querySelector(".side-bar");
-const menuBtn = document.querySelector(".hamburger");
-
-menuBtn.addEventListener("click",()=>{
-    console.log("click");
-    sideBar.classList.toggle("hide-list");
-});
